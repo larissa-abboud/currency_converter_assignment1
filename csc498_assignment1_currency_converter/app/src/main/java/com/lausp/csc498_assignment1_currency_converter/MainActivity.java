4a414a6c704673fef28744fe3bd538f5d095a05c
Larissa Abboud <lariabboud@gmail.com>
2022-02-02 21:00:19 +0200

    added java code

--- csc498_assignment1_currency_converter/app/src/main/java/com/lausp/csc498_assignment1_currency_converter/MainActivity.java
+++ csc498_assignment1_currency_converter/app/src/main/java/com/lausp/csc498_assignment1_currency_converter/MainActivity.java
@@ -3,18 +3,38 @@ package com.lausp.csc498_assignment1_currency_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-     EditText lbp;
-     EditText dollar;
+     EditText addAmount;
+     TextView result;
+     Button convertToLBP;
+     Button convertToDollars;
+     int results;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dollar = (EditText) findViewById(R.id.second);
-        lbp = (EditText) findViewById(R.id.first);
+        addAmount = (EditText) findViewById(R.id.first);
+        result = (TextView) findViewById(R.id.result);
+        convertToLBP = (Button) findViewById(R.id.button2);
+        convertToDollars = (Button) findViewById(R.id.button3);
+
+
+
+    }
+    public void convertLbp(View v){
+       int amount =  Integer.parseInt(addAmount.getText().toString());
+        results = amount * 22000;
+        
+
+
+
     }
 }
