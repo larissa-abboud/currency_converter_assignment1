aed99baa7c37366d55198131ff5e0247fa07ffd5
Larissa Abboud <lariabboud@gmail.com>
2022-02-02 15:31:59 +0200

    first commit

---
+++ csc498_assignment1_currency_converter/app/src/main/java/com/lausp/csc498_assignment1_currency_converter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lausp.csc498_assignment1_currency_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// EditText lbp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
